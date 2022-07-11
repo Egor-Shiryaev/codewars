82868168fce54958f38ea8898f48c70f9109ebb8
Egor-Shiryaev <euro24rus@gmail.com>
2022-07-11 15:11:38 +0700

    inite: add files

---
+++ 8 kyu/How good are you really.js
@@ -0,0 +1,18 @@
+"use strict";
+// https://www.codewars.com/kata/5601409514fc93442500010b/train/javascript
+
+
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+
+function betterThanAverage(classPoints, yourPoints) {
+    // Your code here
+  }
+
+
+  console.log(function betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9));
